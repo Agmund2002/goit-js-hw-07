57b68555dd98b4426dc4eb9e8601895d4a441a9f
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-04 20:01:29 +0300

    Створено розмітку для 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const gallery = document.querySelector('.gallery');
+
+const markupGallery = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="s${preview}" alt="${description}" />
+    </a>
+ </li>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+// Додавання функціоналу
+
