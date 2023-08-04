22815205973f1d13a36fef3b778a699c27f42334
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-04 21:18:42 +0300

    Виконав 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,7 +8,7 @@ const gallery = document.querySelector('.gallery');
 const markupGallery = galleryItems.map(({preview, original, description}) => 
     `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
-       <img class="gallery__image" src="s${preview}" alt="${description}" />
+       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
  </li>`
 ).join('');
@@ -17,3 +17,9 @@ gallery.insertAdjacentHTML('beforeend', markupGallery);
 
 // Додавання функціоналу
 
+gallery.addEventListener('click', evt => evt.preventDefault());
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
